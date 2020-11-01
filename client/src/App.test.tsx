a66b59e243a8cd0e8a6efd20b83572421e35781a
Berenger Salmon <bsalmon@kaliop.com>
2020-11-01 23:48:42 +0100

    Init config on front side with react and typescript

---
+++ client/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = document.getElementsByClassName('App')
+  expect(linkElement[0]).toBeInTheDocument();
+});
